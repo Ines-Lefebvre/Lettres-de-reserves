097fb64cc7de84013a047eb56b1fc7d675584060
Ines-Lefebvre <franck.lapuyade@atexya.com>
2025-09-30 12:39:37 +0200

    Add default export to ValidationPage

---
+++ src/pages/ValidationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ValidationPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Validation Page</h1>
+      <p>This is a placeholder page.</p>
+    </div>
+  );
+};
+
+export default ValidationPage;
